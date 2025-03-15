3de8e60531c3139681ff9e09c9208ce3eae7bf0c
austinsenv <austins.systems@protonmail.com>
2025-03-15 10:57:00 -0500

    initial commit: provision vm with terraform

---
+++ terraform/debian-vm.tf
@@ -0,0 +1,39 @@
+# Debian 12 VM with cloud-init
+
+terraform {
+  required_providers {
+    proxmox = {
+      source = "telmate/proxmox"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url          = var.pm_api_url
+  pm_api_token_id     = var.pm_api_token_id
+  pm_api_token_secret = var.pm_api_token_secret
+  pm_tls_insecure     = true
+}
+
+resource "proxmox_vm_qemu" "debian-vm" {
+  name        = "debian-vm"
+  target_node = "pve-1"
+  clone       = "template-vm01"
+  full_clone  = true
+  cores       = 4
+  memory      = 8192
+
+  disk {
+    size     = "32G"
+    type     = "scsi"
+    storage  = "local-lvm"
+    discard  = "on"
+  }
+
+  network {
+    model     = "virtio"
+    bridge    = "vmbr0"
+    firewall  = false
+    link_down = false
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+# these variables are sourced from the shell's environment
+
+variable "pm_api_url" {
+  description = "Proxmox API URL"
+  type        = string
+}
+
+variable "pm_api_token_id" {
+  description = "Proxmox API Token ID"
+  type        = string
+}
+
+variable "pm_api_token_secret" {
+  description = "Proxmox API Token Secret"
+  type        = string
+  sensitive   = true
+}
